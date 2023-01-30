e4049ff9ae294e6110e1ab2f33f6cf6749dea348
HeroKeepleng <maxmixtet@gmail.com>
2023-01-30 19:40:57 +0200

    Add 3 minigames mode
    Add mode: easy, medium, hard

---
+++ Assets/Scripts/HardMiniGame.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HardMiniGame : MonoBehaviour
+{
+    public Slider miniGameSlider;
+    public Slider reactionCountSlider;
+    public RectTransform pointerContainer;
+    public RectTransform reactionBar;
+
+    public int reactionCount;
+
+    public float reactionLength;
+    public float pointerSpeed;
+
+    private float pointerAngle;
+    private float currentPointerAngle;
+    private float reactionBarAngle;
+    private float currentReactionBarAngle;
+    private float checkReaction;
+    private int checkCount;
+
+    private bool isReverse;
+    private bool stopGame = true;
+
+    void Start()
+    {
+        StartCoroutine(StartMiniGame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopGame)
+        {
+            miniGameSlider.value = (reactionLength * 1) / 360;
+            PointerMove();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (pointerAngle > reactionBarAngle && pointerAngle < checkReaction)
+                {
+                    checkCount++;
+                    reactionCountSlider.value++;
+                    isReverse = !isReverse;
+                    reactionBarAngle = pointerAngle;
+                    currentReactionBarAngle = currentPointerAngle;
+                    SpawnReactionBar();
+                }
+                else
+                {
+                    stopGame = true;
+                }
+            }
+
+            if (pointerAngle > checkReaction)
+            {
+                stopGame = true;
+                Debug.Log("You lose");
+            }
+
+            if (checkCount == reactionCount)
+            {
+                checkCount = 0;
+                stopGame = true;
+                Debug.Log("You win");
+            }
+        }
+    }
+
+    private void PointerMove()
+    {
+        if (!isReverse)
+        {
+            float angleSpeed = Time.deltaTime / pointerSpeed * 360;
+            pointerAngle += angleSpeed;
+            currentPointerAngle += angleSpeed;
+            pointerContainer.localEulerAngles = new Vector3(0, 0, -currentPointerAngle);
+        }else if (isReverse)
+        {
+            float angleSpeed = Time.deltaTime / pointerSpeed * 360;
+            pointerAngle += angleSpeed;
+            currentPointerAngle -= angleSpeed;
+            pointerContainer.localEulerAngles = new Vector3(0, 0, -currentPointerAngle);
+        }
+    }
+
+
+    private void SpawnReactionBar()
+    {
+        float randomRangeRotate = Random.Range(90, 270);
+        if (!isReverse)
+        {
+            reactionBar.localScale = new(1, 1, 1);
+            reactionBarAngle += randomRangeRotate;
+            currentReactionBarAngle += randomRangeRotate;
+            reactionBar.localEulerAngles = new Vector3(0, 0, -currentReactionBarAngle);
+            checkReaction = reactionBarAngle + reactionLength;
+        }else if (isReverse)
+        {
+            reactionBar.localScale = new(-1, 1, 1);
+            reactionBarAngle += randomRangeRotate;
+            currentReactionBarAngle -= randomRangeRotate;
+            reactionBar.localEulerAngles = new Vector3(0, 0, -currentReactionBarAngle);
+            checkReaction = reactionBarAngle + reactionLength;
+        }
+    }
+
+    private IEnumerator StartMiniGame()
+    {
+        pointerContainer.localEulerAngles = Vector3.zero;
+        SpawnReactionBar();
+        reactionCountSlider.maxValue = reactionCount;
+        yield return new WaitForSeconds(1f);
+        stopGame = false;
+    }
+
+    public void RestartGame()
+    {
+        pointerAngle = 0;
+        reactionBarAngle = 0;
+        miniGameSlider.value = 0;
+
+        StartCoroutine(StartMiniGame());
+    }
+}
