c5d18c39f21f419add0d52ff7000a3a7271718ec
LeoGreenfor <leonoraherobrine@gmail.com>
2023-01-31 17:42:09 +0200

    Interaction with roots and altars update

---
+++ Assets/Scripts/PlayerItemStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerItemStats : MonoBehaviour
+{
+    public float distanceToObject;
+    public int countOfRoots;
+    public int countOfPuppet;
+    public int countOfUpgratedPuppet;
+}
