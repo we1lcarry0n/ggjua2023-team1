7ea8ea2ebeb6eff01bd221eca57f3a0a76566bc7
LeoGreenfor <leonoraherobrine@gmail.com>
2023-02-01 16:29:18 +0200

    Update SpawnRoots.cs

--- Assets/Scripts/SpawnRoots.cs
+++ Assets/Scripts/SpawnRoots.cs
@@ -1,43 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnRoots : MonoBehaviour
 {
     public Vector3[] spawnPoints;
-    public int numberOfRootsOnScene;
+    public GameObject rootToSpawn;
+    public Dictionary<int, int> scenesValueRoots = new()
+    {
+        {1, 2},
+        {2, 3},
+        {3, 4},
+        {4, 3},
+        {5, 4}
+    };
 
-    // Start is called before the first frame update
     void Start()
     {
+        //int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int currentScene = 1;
+        Debug.Log(currentScene);
+        if (scenesValueRoots.TryGetValue(currentScene, out int numberOfRootsOnScene))
+        {
+            SetRandomPoints(numberOfRootsOnScene);
+        }
+        Destroy(rootToSpawn);
     }
 
-    private void SetRandomPoints(int n)
+    /// <summary>
+    /// Повертає масив рандомних значень розмірністю n 
+    /// </summary>
+    /// <param name="sizeOfArray">Розмірність масиву</param>
+    private void SetRandomPoints(int sizeOfArray)
     {
         int currentPoint;
-        for (int i = 0; i <=n; i++)
+        for (int i = 0; i < sizeOfArray; i++)
         {
             currentPoint = NewRandomNumber();
+            Instantiate(rootToSpawn, spawnPoints[currentPoint], Quaternion.identity);
         }
     }
 
     private int randomNumber;
     private int lastNumber;
+    /// <summary>
+    /// Генерує рандомне неповторюване число
+    /// </summary>
     private int NewRandomNumber()
     {
-        randomNumber = Random.Range(0, spawnPoints.Length);
+        randomNumber = Random.Range(0, spawnPoints.Length-1);
         if (randomNumber == lastNumber)
         {
-            randomNumber = Random.Range(0, spawnPoints.Length);
+            randomNumber = Random.Range(0, spawnPoints.Length-1);
         }
         lastNumber = randomNumber;
 
         return lastNumber;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
