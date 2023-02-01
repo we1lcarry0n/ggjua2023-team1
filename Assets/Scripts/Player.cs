6b91fdd39d10205e3562c2c0cbfe1b8a3d074999
we1lcarry0n <andriy.fed@gmail.com>
2023-02-01 06:15:04 +0200

    Added animations and merged everything up
    Flipping coin, i want to sleep soooooooo bad

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -8,34 +10,74 @@ public class Player : MonoBehaviour
     public float rotationSpeed;
     public float force;
     public float sprint;
+    public float gravity;
 
-    private Rigidbody rb;
+    private Vector3 movementDirection;
+
+    [SerializeField] private Animator animator;
+    private CharacterController chc;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        chc = GetComponent<CharacterController>();
     }
 
-    private void FixedUpdate()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        transform.position += transform.forward * z * speed;
-
-        transform.Rotate(0, x * rotationSpeed, 0);
-    }
 
     private void Update()
     {
+        if (chc.isGrounded)
+        {
+
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        movementDirection = Vector3.zero;
+        Debug.Log(chc.isGrounded);
+        
+        
+        movementDirection = x * Vector3.right + z * Vector3.forward;
+
+        movementDirection.Normalize();
+        ApplyGravity();
+
+        if (movementDirection.z == 0 && movementDirection.x == 0)
+        {
+            animator.SetFloat("Speed", 0, .1f, Time.deltaTime);
+        }
+        else if (movementDirection == Vector3.zero)
+        {
+            animator.SetFloat("Speed", .15f, .1f, Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 1, .1f, Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime*rotationSpeed);
+        }
+
+        chc.Move(movementDirection * speed * Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            rb.AddForce(transform.forward * force, ForceMode.Impulse);
+            //rb.AddForce(transform.forward * force, ForceMode.Impulse);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            rb.AddForce(transform.forward * sprint);
+            //rb.AddForce(transform.forward * sprint);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (chc.isGrounded)
+        {
+            movementDirection.y = -gravity * .02f;
+        }
+        else
+        {
+            movementDirection.y = - gravity * Time.deltaTime;
         }
     }
 }
