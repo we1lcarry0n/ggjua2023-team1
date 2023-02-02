d6b961a04fff57732fde9df179216240057babec
we1lcarry0n <andriy.fed@gmail.com>
2023-02-02 06:33:05 +0200

    Added stamina + UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
@@ -9,6 +10,13 @@ public class Player : MonoBehaviour
     public float baseSpeed;
     public float rotationSpeed;
     public float sprint;
+
+    [SerializeField] private float maxStamina;
+    [SerializeField] private float staminaConsumption;
+    [SerializeField] private float staminaRegen;
+    [SerializeField] private Image staminaBar;
+    private float currentStamina;
+    private bool isSprinting;
 
     private Vector3 movementDirection;
 
@@ -18,6 +26,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         nma = GetComponent<NavMeshAgent>();
+        currentStamina = maxStamina;
     }
 
     private void Update()
@@ -29,10 +38,14 @@ public class Player : MonoBehaviour
         movementDirection = x * Vector3.right + z * Vector3.forward;
 
         AdjustAnimation();
-
-        float speed = Input.GetKey(KeyCode.LeftShift) ? baseSpeed*sprint : baseSpeed;
+        SprintCheck();
+        
+        float speed = isSprinting ? baseSpeed*sprint : baseSpeed;
 
         nma.Move(movementDirection * Time.deltaTime * speed);
+
+        UpdateStamina();
+        RefreshUI();
     }
 
     private void AdjustAnimation()
@@ -50,5 +63,47 @@ public class Player : MonoBehaviour
             animator.SetFloat("Speed", Input.GetKey(KeyCode.LeftShift) ? 1f : .65f, .1f, Time.deltaTime);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(movementDirection), Time.deltaTime * rotationSpeed);
         }
+    }
+
+    private void SprintCheck()
+    {
+        isSprinting = (Input.GetKey(KeyCode.LeftShift) && currentStamina != 0);
+    }
+
+    public void ConsumeStamina(float staminaConsumptionAmount)
+    {
+        currentStamina -= staminaConsumptionAmount;
+        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+    }
+
+    public void RegenerateStamina(float staminaRegenAmount)
+    {
+        currentStamina += staminaRegenAmount;
+        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+    }
+    private void UpdateStamina()
+    {
+        if (isSprinting)
+        {
+            ConsumeStamina(staminaConsumption * Time.deltaTime);
+        }
+        else if (!isSprinting && !Input.GetKey(KeyCode.LeftShift))
+        {
+            RegenerateStamina(staminaRegen * Time.deltaTime);
+        }
+
+    }
+
+    private void RefreshUI()
+    {
+        if(currentStamina == maxStamina && staminaBar.enabled)
+        {
+            staminaBar.enabled = false;
+        }
+        else if(!staminaBar.enabled && currentStamina < maxStamina)
+        {
+            staminaBar.enabled = true;
+        }
+        staminaBar.fillAmount = currentStamina / maxStamina;
     }
 }
