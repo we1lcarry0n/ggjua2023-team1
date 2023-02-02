713f48f08230d6bf55d0274ec015469b878f7454
we1lcarry0n <andriy.fed@gmail.com>
2023-02-02 05:12:40 +0200

    Fixed camera upward assest

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform camera;
+    [SerializeField] private float upwardOffset;
     [SerializeField] private float forwardOffset;
 
     private void LateUpdate()
     {
-        camera.position = new Vector3(transform.position.x, camera.position.y, transform.position.z - forwardOffset);
+        camera.position = new Vector3(transform.position.x, transform.position.y + upwardOffset, transform.position.z - forwardOffset);
     }
 }
